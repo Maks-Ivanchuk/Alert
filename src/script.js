e67ccdf4c8a9742fd81505e24f5270f6d3503c27
Maks Ivanchuk <maksivanchu@gmail.com>
2023-05-12 11:56:42 +0300

    ad

--- src/script.js
+++ src/script.js
@@ -1,49 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
-   debugger
-   document.body.addEventListener("click", function (event) {
-      let target = event.target;
+document.querySelectorAll('.accordion__title').forEach((elem) => {
+   elem.addEventListener('click', () => {
 
-      target.closest('.components__collapse').querySelectorAll('.accordion__title').forEach((elem) => {
+      let content = elem.nextElementSibling;
 
-         elem.addEventListener('click', () => {
-
-            let content = elem.nextElementSibling;
-
-            if (content.style.maxHeight) {
-               document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
-               content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(0deg)";
-            }
-            else {
-               document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
-               content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(180deg)";
-               content.style.maxHeight = content.scrollHeight + "px";
-            };
-         });
-      });
+      if (content.style.maxHeight) {
+         document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
+         content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(0deg)";
+      }
+      else {
+         document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
+         content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(180deg)";
+         content.style.maxHeight = content.scrollHeight + "px";
+      };
    });
-   // document.querySelectorAll('.accordion__title').forEach((elem) => {
-   //    elem.addEventListener('click', () => {
-
-   //       let content = elem.nextElementSibling;
-
-   // if (content.classList.contains("accordion__content--active") == false) {
-   //    content.classList.add("accordion__content--active");
-   //    console.log("active");
-   // } else {
-   //    content.classList.remove("accordion__content--active");
-   //    console.log("none");
-   // }
-
-   //    if (content.style.maxHeight) {
-   //       document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
-   //       content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(0deg)";
-   //    }
-   //    else {
-   //       document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
-   //       content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(180deg)";
-   //       content.style.maxHeight = content.scrollHeight + "px";
-   //    }
-
-   // })
-   // })
 });
