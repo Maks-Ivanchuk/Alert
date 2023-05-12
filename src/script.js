d41443c6394dc9c378a4f19820775947467489b8
Maks Ivanchuk <maksivanchu@gmail.com>
2023-05-12 13:45:02 +0300

    end

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ document.querySelectorAll('.accordion__title').forEach((elem) => {
 
       if (content.style.maxHeight) {
          document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
-         content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(0deg)";
+         //content.closest('.collapse__accordion').querySelector('.accordion__button-icon').style.transform = "rotate(0deg)";
       }
       else {
          document.querySelectorAll('.accordion__content').forEach((elem) => elem.style.maxHeight = null);
