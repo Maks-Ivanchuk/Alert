b5ee5c5854c21519b5adc2b2554419d499bc3301
Maks Ivanchuk <maksivanchu@gmail.com>
2023-05-23 16:27:07 +0300

    2

--- src/script.js
+++ src/script.js
@@ -1,50 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-   const btnOpenModal = document.querySelectorAll('[data-btnOpenModal]');
-   const btnCancelModal = document.querySelectorAll('[data-btnCancelModal]');
-   const btnConfirmModal = document.querySelectorAll('[data-btnConfirmModal]');
-   const overlay = document.querySelector('#overlay');
-   const body = document.body;
+   $(".slider").slick(function () { });
 
-   function openModalWindows(modal) {
-      if (modal == null) return;
-      modal.classList.add("active");
-      overlay.classList.add("active");
-   };
-
-   function cloceModalWindows(modal) {
-      if (modal == null) return;
-      modal.classList.remove("active");
-      overlay.classList.remove("active");
-   };
-
-   function confirmModalWindows(confirm) {
-      if (confirm == null) return;
-      alert('To close the window, press "x" or "Cancel"');
-   };
-
-   btnOpenModal.forEach(button => {
-      button.addEventListener('click', (event) => {
-         const modal = event.target.nextElementSibling;
-         openModalWindows(modal);
-         body.style.overflow = "hidden";
-      });
+   $("img").click(function () {
+      $(this).hide();
    });
-
-   btnCancelModal.forEach(button => {
-      button.addEventListener('click', () => {
-         const modal = button.closest('.modal');
-         cloceModalWindows(modal);
-         body.style.overflow = "visible";
-      })
-   });
-
-   btnConfirmModal.forEach(confirm => {
-      confirm.addEventListener('click', () => {
-         confirmModalWindows(confirm);
-      });
-   });
-
-   // slider ---------------------------------------
-
-
 });
