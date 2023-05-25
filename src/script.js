4a9cd7b8a5b4d4b02047fe06185cc21378f3ffbe
Maks Ivanchuk <maksivanchu@gmail.com>
2023-05-25 15:12:14 +0300

    dsfsdg

--- src/script.js
+++ src/script.js
@@ -1,5 +1,48 @@
 document.addEventListener("DOMContentLoaded", () => {
-   $('.slider').slick();
+   $('.slider').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      fade: true,
+      asNavFor: '.slider-second',
+      responsive: [
+         {
+            breakpoint: 769,
+            settings: {
+               arrows: false,
+            }
+         },
+      ]
+   });
+
+   $('.slider-second').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      centerMode: true,
+      centerPadding: '60px',
+      focusOnSelect: true,
+      asNavFor: '.slider',
+      dots: false,
+      pauseOnHover: true,
+      pauseOnDotsHover: true,
+      arrows: false,
+      responsive: [
+         {
+            breakpoint: 769,
+            settings: {
+               slidesToShow: 2,
+               dots: true,
+            }
+         },
+         {
+            breakpoint: 376,
+            settings: {
+               slidesToShow: 1,
+               dots: true,
+            }
+         },
+      ]
+   });
 });                                                              //працює
 //-------------------------------------------------------------------------------
 
