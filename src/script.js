c79de12e0e68c3886f569097b25d5f6cea1dd0a2
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-06-04 22:15:47 +0300

    sda

--- src/script.js
+++ src/script.js
@@ -1,17 +1,46 @@
 document.addEventListener("DOMContentLoaded", () => {
    let form = document.querySelector('#form');
 
+   let gettingUsersName = document.querySelector('#gettingUsersName');
+   
+   gettingUsersName.addEventListener('ckick', function (event) {
+      fetch('https://jsonplaceholder.typicode.com/users', {
+         method: 'POST',
+         body: JSON.stringify({
+            id: 'test',
+            name: 'test',
+            username: 'test',
+            company: {
+               name: 'test',
+            }
+         }),
+         headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+         },
+      })
+         .then((response) => response.json())
+         .then((response) => {
+            console.log(response);
+            response.innerHTML = response.userId;
+         });
+   });
+   
+
    form.addEventListener('submit', function (event) {
 
       event.preventDefault();
 
       let userName = document.querySelector('#userName').value;
 
-
-      fetch('https://jsonplaceholder.typicode.com/posts', {
+      fetch('https://jsonplaceholder.typicode.com/users', {
          method: 'POST',
          body: JSON.stringify({
-            title: userName,
+            id: '111',
+            name: '1111',
+            username: userName,
+            company: {
+               name: '111',
+            }
          }),
          headers: {
             'Content-type': 'application/json; charset=UTF-8',
@@ -22,7 +51,7 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log(dataUser);
             let results = document.querySelector('#jsonResult')
 
-            results.innerHTML = `<h3>User name: ${dataUser.title}</h3>`;
+            results.innerHTML = `<h3>User name: ${dataUser.username}</h3>`;
          });
    });
 });
