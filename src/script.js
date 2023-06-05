6d89590015cb46937951575a57c7b6dac9983dbd
Maks Ivanchuk <maksivanchu@gmail.com>
2023-06-05 16:01:38 +0300

    фівфів

--- src/script.js
+++ src/script.js
@@ -1,57 +1,50 @@
 document.addEventListener("DOMContentLoaded", () => {
-   let form = document.querySelector('#form');
+   let gettingUsers = document.querySelector('#gettingUsers');
+   let resultTitle = document.querySelector('#result__title');
+   let resultValue = document.querySelector('#result__value');
 
-   let gettingUsersName = document.querySelector('#gettingUsersName');
-   
-   gettingUsersName.addEventListener('ckick', function (event) {
-      fetch('https://jsonplaceholder.typicode.com/users', {
-         method: 'POST',
-         body: JSON.stringify({
-            id: 'test',
-            name: 'test',
-            username: 'test',
-            company: {
-               name: 'test',
-            }
-         }),
-         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-         },
-      })
-         .then((response) => response.json())
-         .then((response) => {
-            console.log(response);
-            response.innerHTML = response.userId;
-         });
-   });
-   
+   document.body.addEventListener('click', function (event) {
+      let target = event.target;
 
-   form.addEventListener('submit', function (event) {
+      if (target.id == "gettingUsers") {
 
-      event.preventDefault();
+         target.classList.add('button--success');
 
-      let userName = document.querySelector('#userName').value;
+         fetch('https://jsonplaceholder.typicode.com/users')
 
-      fetch('https://jsonplaceholder.typicode.com/users', {
-         method: 'POST',
-         body: JSON.stringify({
-            id: '111',
-            name: '1111',
-            username: userName,
-            company: {
-               name: '111',
-            }
-         }),
-         headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-         },
-      })
-         .then((response) => response.json())
-         .then((dataUser) => {
-            console.log(dataUser);
-            let results = document.querySelector('#jsonResult')
+            .then((response) => response.json())
+            .then((dataUser) => {
+               console.log(dataUser);
 
-            results.innerHTML = `<h3>User name: ${dataUser.username}</h3>`;
-         });
+               let i = 0;
+
+               dataUser.forEach(element => {
+                  console.log(i);
+                  console.log(dataUser[i]);
+
+                  resultTitle.innerHTML =
+                     `<p>
+                     Id:<br>
+                     Name:<br>
+                     User name:<br>
+                     Company name:<br>
+                  </p>`;
+
+                  resultValue.innerHTML =
+                     `<p>
+                     ${dataUser[i].id}<br>
+                     ${dataUser[i].name}<br>
+                     ${dataUser[i].username}<br>
+                     ${dataUser[i].company.name}<br>
+                  </p>`;
+
+                  i++;
+               });
+            });
+      }
+
+      if (target.id == "gettingRequest") {
+         target.classList.add('button--success');
+      }
    });
 });
