45d9836513669834d5767e0204e706b5194a20a3
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-06-20 23:16:15 +0300

    sdf

--- src/script.js
+++ src/script.js
@@ -23,80 +23,120 @@ document.addEventListener("DOMContentLoaded", () => {
       .then((response) => response.json())
       .then((users) => {
 
-      if (users == null) {
-         return;
-      } else {
-         users.forEach(user => {
-            tableUsers.insertAdjacentHTML('beforeend', `
-               <tr>
-                  <td data-user-id="${user.id}">${user.id}</td>
-                  <td>${user.name}</td>
-                  <td>${user.username}</td>
-                  <td>${user.email}</td>
-                  <td>${user.address.city}</td>
-                  <td>${user.phone}</td>
-                  <td>${user.website}</td>
-                  <td>${user.company.name}</td>
-                  <td>
-                     <button data-user-id="${user.id}" class = "button-todos button button--danger" style="margin: 5px;">Todos</button>
-                  </td>
-               </tr>
-            `);
-         });
-      };
-      const buttons = document.querySelectorAll('button[data-user-id]');
+      users.forEach(user => {
+         tableUsers.insertAdjacentHTML('beforeend', `
+            <tr>
+               <td data-user-id="${user.id}">${user.id}</td>
+               <td>${user.name}</td>
+               <td>${user.username}</td>
+               <td>${user.email}</td>
+               <td>${user.address.city}</td>
+               <td>${user.phone}</td>
+               <td>${user.website}</td>
+               <td>${user.company.name}</td>
+               <td>
+                  <button data-user-id="${user.id}" class = "button button--danger" style="margin: 5px;">Todos</button>
+               </td>
+            </tr>
+         `);
+      });
+      const buttonsUsers = document.querySelectorAll('button[data-user-id]');
    
-      buttons.forEach(button => {
-         button.addEventListener('click', function (event) { 
+      buttonsUsers.forEach(buttonUser => {
+         buttonUser.addEventListener('click', function (event) { 
             let target = event.target;
+            const userId = target.dataset.userId;
+            const tableResult1 = document.querySelector('#result1');
             
-            if (target.classList.contains('button-todos')) {
-               const userId = target.dataset.userId;
-               const tableResult1 = document.querySelector('#result1');
+            tableResult1.classList.add('result--active');
+            
+            fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
+            .then((response) => response.json())
+            .then((todosUser) => {
+               todosUser.forEach(todo => {
+                  tableResult1.insertAdjacentHTML('beforeend', `
+                     <tr>
+                        <td data-user-id="${todo.userId}">${todo.userId}</td>
+                        <td>${todo.id}</td>
+                        <td>${todo.title}</td>
+                        <td>${todo.completed}</td>
+                        <td>
+                           <button data-user-info="${userId}" class = "button-posts button button--info" style="margin: 5px;">Posts > coments</button>
+                           <button data-user-info="${userId}" class = "button-albums button button--warning" style="margin 5px;">Albums > photo</button>
+                        </td>
+                     </tr>
+                  `);
+               });
                
-               tableResult1.classList.add('result1--active');
-               
-               fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
-                  .then((response) => response.json())
-                  .then((todosUser) => {
-                     todosUser.forEach(todo => {
-                        tableResult1.insertAdjacentHTML('beforeend', `
-                        <tr>
-                           <td data-user-id="${todo.userId}">${todo.userId}</td>
-                           <td>${todo.id}</td>
-                           <td>${todo.title}</td>
-                           <td>${todo.completed}</td>
-                           <td>
-                              <button data-user-id="${userId}" class = "button-posts button button--info" style="margin: 5px;">Posts</button>
-                              <button data-user-id="${userId}" class = "button-albums button button--warning" style="margin 5px;">Albums</button>
-                           </td>
-                        </tr>
-                     `);
-                     })
-                  })
-            }
+               const buttonUserInfo = document.querySelectorAll('button[data-user-info]');
+
+               buttonUserInfo.forEach(btnUserInfo => { 
+                  btnUserInfo.addEventListener('click', function (event) {
+                     let target = event.target;
+                     const tableResult2 = document.querySelector('#result2');
+
+                     if (target.classList.contains('button-posts')) { 
+                        
+                     tableResult2.insertAdjacentHTML('beforeend', `
+                           <tr>
+                              <th>Post Id</th>
+                              <th>Id</th>
+                              <th>Name</th>
+                              <th>Email</th>
+                              <th>Body</th>
+                           </tr>
+                        `);
+                        tableResult2.classList.add('result--active');
+
+                        fetch(`https://jsonplaceholder.typicode.com/posts/${userId}/comments`)
+                           .then((response) => response.json())
+                           .then((comentsUser) => {
+                              comentsUser.forEach(coments => {
+                                 tableResult2.insertAdjacentHTML('beforeend', `
+                                    <tr>
+                                       <td>${coments.postId}</td>
+                                       <td>${coments.id}</td>
+                                       <td>${coments.name}</td>
+                                       <td>${coments.email}</td>
+                                       <td>${coments.body}</td>
+                                    </tr>
+                                 `);
+                              })
+                           });
+                     };
+                     if (target.classList.contains('button-albums')) { 
+                                                
+                        tableResult2.classList.add('result--active');
+                        tableResult2.insertAdjacentHTML('beforeend', `
+                                    <tr>
+                                       <th>Album Id}</th>
+                                       <th>Id</th>
+                                       <th>Title</th>
+                                       <th>Url</th>
+                                       <th>Thumbnail url</th>
+                                    </tr>
+                                 `);
+
+                        fetch(`https://jsonplaceholder.typicode.com/albums/${userId}/photos`)
+                           .then((response) => response.json())
+                           .then((albumsUser) => {
+                              albumsUser.forEach(album => {
+                                 tableResult2.insertAdjacentHTML('beforeend', `
+                                    <tr>
+                                       <td>${album.albumId}</td>
+                                       <td>${album.id}</td>
+                                       <td>${album.title}</td>
+                                       <td>${album.url}</td>
+                                       <td>${album.thumbnailUrl}</td>
+                                    </tr>
+                                 `);
+                              })
+                           });
+                     };
+                  });
+               });
+            });
          });
       });
    });
 });
-
-
-// const buttons = document.querySelectorAll('button[data-user-id]');
-
-//             buttons.forEach(button => {
-//                button.addEventListener('click', function (event) {
-//                   event.preventDefault();
-//                   const userId = event.target.dataset.userId;
-//                   const todosWrapper = document.querySelector(`.todos[data-user-id="${userId}"]`);
-
-//                fetch(`https://jsonplaceholder.typicode.com/users/${userId}/todos`)
-//                   .then((response) => response.json())
-//                   .then((dataUser) => {
-//                      dataUser.forEach(todo => {
-//                         todosWrapper.insertAdjacentHTML('beforeend', `
-//                            <div>ID:${todo.id}; TITLE:${todo.title}; COMPLETED:${todo.completed}</div>
-//                         `);
-//                      })
-//                   });
-//                   })
-//             });
