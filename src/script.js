6154c49c618578e06eadafae68e24f83aa112ebf
Maks-Ivanchuk <maksivanchu@gmail.com>
2023-07-18 17:57:56 +0300

    asdasd

--- src/script.js
+++ src/script.js
@@ -1,226 +1,205 @@
 // document.addEventListener("DOMContentLoaded", () => {
-//    const calc = document.querySelector('.calculator');
-//    const calcResult = document.querySelector('.calculator__result');
-//    const input1 = document.querySelector('#input1');
-//    const input2 = document.querySelector('#input2');
-//    let countRow = 0;
-//    let validation = true;
+   // const numButtons = document.querySelectorAll('[data-number]');
+   // const operationButtons = document.querySelectorAll('[data-operation]');
+   // const currentOperation = document.querySelector('.calculator__current');
+   // const delOneEl = document.querySelector('[data-delete]');
+   // const delAllEl = document.querySelector('[data-all-clear]');
 
-//    calc.addEventListener('click', function (event) {
-//       let target = event.target;
-//       let sum;
+   // let inputNumber = null;
+   // let sign = null;
+   // let sum;
 
-//       if (target.id == 'addition' && validation == true) {
-//          sum = Number(input1.value) + Number(input2.value);
+   // function valueAssignment(input) {
+   //    if (inputNumber == null) {
+   //       inputNumber = input;
+   //       return inputNumber = Number(inputNumber);
+   //    } else {
+   //       inputNumber += input;
+   //       return inputNumber = Number(inputNumber);
+   //    };
+   // };
 
-//          calcResult.insertAdjacentHTML('afterbegin', `
-//             <tr>
-//                <td style = "width:15%;">Result: </td>
-//                <td id="targetCopyResult";>${sum}</td>
-//             </tr>
-//          `);
+   // function definitionSign(input) {
+   //    if (sign != false) return false;
+   //    sign = Number(input)
+   //    return true;
+   // };
 
-//          countRow++;
-//       };
+   // function outputOnScreen(val) {
+   //    currentOperation.innerText += val;
+   // };
 
-//       if (target.id == 'subtraction' && validation == true) {
-//          sum = Number(input1.value) - Number(input2.value);
+   // function delOneElement() {
+   //    currentOperation.innerText = currentOperation.innerText.slice(0, -1);
+   // };
 
-//          calcResult.insertAdjacentHTML('afterbegin', `
-//             <tr>
-//                <td style = "width:15%;">Result: </td>
-//                <td id="targetCopyResult";>${sum}</td>
-//             </tr>
-//          `);
+   // function delAllElements() {
+   //    currentOperation.innerText = '';
+   // };
 
-//          countRow++;
-//       };
+   // numButtons.forEach(button => {
+   //    button.addEventListener('click', function () {
+   //       valueAssignment(this.value);
+   //       outputOnScreen(this.value);
+   //       console.log(inputNumber);
+   //    });
+   // });
 
-//       if (target.id == 'division' && validation == true) {
-//          sum = Number(input1.value) / Number(input2.value);
+   // operationButtons.forEach(operant => {
+   //    operant.addEventListener('click', function () {
+   //       outputOnScreen(this.value);
+   //       inputNumber = null;
+   //    });
+   // });
 
-//          calcResult.insertAdjacentHTML('afterbegin', `
-//             <tr>
-//                <td style = "width:15%;">Result: </td>
-//                <td id="targetCopyResult";>${sum}</td>
-//             </tr>
-//          `);
+   // delOneEl.onclick = function () {
+   //    delOneElement();
+   // }
 
-//          countRow++;
-//       };
+   // delAllEl.onclick = function () {
+   //    delAllElements();
+   // }
+   // });
+class Calculator {
+   constructor(previousOperandTextElement, currentOperandTextElement) {
+      this.previousOperandTextElement = previousOperandTextElement;
+      this.currentOperandTextElement = currentOperandTextElement;
+      this.clear();
+   };
 
-//       if (target.id == 'multiplication' && validation == true) {
-//          sum = Number(input1.value) * Number(input2.value);
+   clear() { 
+      this.currentOperand = '';
+      this.previousOperand = '';
+      this.operation = undefined;
+   };
 
-//          calcResult.insertAdjacentHTML('afterbegin', `
-//             <tr>
-//                <td style = "width:15%;">Result: </td>
-//                <td id="targetCopyResult";>${sum}</td>
-//             </tr>
-//          `);
 
-//          countRow++;
-//       };
+   delete() {
+      this.currentOperand = this.currentOperand.toString().slice(0, -1);
+   };
 
-//       if (target.id == 'cleanResult') {
-//          calcResult.classList.remove('result--active');
-//          for (let i = 0; i < countRow; i++) {
-//             calcResult.deleteRow(0);
-//          };
+   getDisplayNumber(number) {
+      // const floatNumber = parseFloat(number);
+      
+      // if (isNaN(floatNumber)) return '';
+      
+      // return floatNumber.toLocaleString('en');
 
-//          countRow = 0;
-//       };
+      const stringNumber = number.toString();
+      const integerDigits = parseFloat(stringNumber.split('.')[0]);
+      const decimalDigits = stringNumber.split('.')[1];
+      let integerDisplay;
 
-//       if (target.id == 'clean') {
-//          input1.value = '';
-//          input2.value = '';
-//          input1.classList.remove("input-invalid");
-//          input2.classList.remove("input-invalid");
-//       };
-
-//       if (target.tagName == 'BUTTON' && target.id != 'cleanResult' && target.id != 'clean') {
-
-//          if (!calcResult.classList.contains('result--active')) {
-//             calcResult.classList.add('result--active');
-//          };
-
-//          if (validation == false) {
-//             input1.value = '';
-//             input2.value = '';
-//             input1.classList.remove("input-invalid");
-//             input2.classList.remove("input-invalid");
-
-//             calcResult.insertAdjacentHTML('afterbegin', `
-//                <tr>
-//                   <td style = "width:15%;">Result: </td>
-//                   <td id="targetCopyResult";>Invalid properties</td>
-//                </tr>
-//             `);
-
-//             countRow++;
-//             validation = true;
-//          } else {
-//             input1.value = '';
-//             input2.value = '';
-//             input1.classList.remove("input-invalid");
-//             input2.classList.remove("input-invalid");
-//          };
-//       };
-//    });
-
-//    const table = document.querySelector('table');
-
-//    table.addEventListener('click', function (event) {
-//       let target = event.target;
-
-//       if (target.id == 'targetCopyResult') {
-//          let copyResult = Number(target.innerText);
-
-//          navigator.clipboard.writeText(copyResult)
-//             .then(() => {
-//                console.log(`Result: ${copyResult} - copied `);
-//             });
-//       };
-//    });
-// });
-
-// let valid1 = true;
-// let valid2 = true;
-
-// function customValid(valueInput) {
-
-//    let value = Number(valueInput.value);
-//    let inputTarget = valueInput;
-
-//    if (inputTarget.id == "input1") {
-//       console.log(value);
-
-//       if (value > 1 && value < 3) {
-//          inputTarget.classList.add("input-invalid");
-//          valid1 = false;
-//       } else {
-//          inputTarget.classList.remove("input-invalid");
-//          valid1 = true;
-//       };
-//       console.log(valid1);
-//    } else if (inputTarget.id == "input2") {
-//       console.log(value);
-
-//       if (value > 1 && value < 3) {
-//          inputTarget.classList.add("input-invalid");
-//          valid2 = false;
-//       } else {
-//          inputTarget.classList.remove("input-invalid");
-//          valid2 = true;
-//       };
-//       console.log(valid2);
-//    };
-
-//    if (valid1 == true && valid2 == true) {
-//       return true;
-//    } else {
-//       return false;
-//    };
-// };
-document.addEventListener("DOMContentLoaded", () => {
-   const numButtons = document.querySelectorAll('[data-number]');
-   const operationButtons = document.querySelectorAll('[data-operation]');
-   const currentOperation = document.querySelector('.calculator__current');
-   const delOneEl = document.querySelector('[data-delete]');
-   const delAllEl = document.querySelector('[data-all-clear]');
-
-   let inputNumber = null;
-   let sign = null;
-   let sum;
-
-   function valueAssignment(input) {
-      if (inputNumber == null) {
-         inputNumber = input;
-         return inputNumber = Number(inputNumber);
+      if (isNaN(integerDigits)) {
+         integerDisplay = '';
       } else {
-         inputNumber += input;
-         return inputNumber = Number(inputNumber);
+         integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+      };
+
+      if (decimalDigits != null) {
+         return `${integerDisplay}.${decimalDigits}`;
+      } else {
+         return integerDisplay;
       };
    };
 
-   function definitionSign(input) {
-      if (sign != false) return false;
-      sign = Number(input)
-      return true;
+   appendNumber(number) { 
+      if (number === '.' && this.currentOperand.includes('.')) return
+      this.currentOperand = this.currentOperand.toString() + number.toString();
    };
 
-   function outputOnScreen(val) {
-      currentOperation.innerText += val;
+   chooseOperation(operation) {
+      if (this.currentOperand === '') return;
+      if (this.previousOperand !== '') {
+         console.log('operator');
+         this.compute();
+      };
+
+      this.operation = operation;
+      this.previousOperand = this.currentOperand;
+      this.currentOperand = '';
    };
 
-   function delOneElement() {
-      currentOperation.innerText = currentOperation.innerText.slice(0, -1);
+   compute() {
+      let computation;
+      const prev = parseFloat(this.previousOperand);
+      const current = parseFloat(this.currentOperand);
+      
+      if (isNaN(prev) || isNaN(current)) return;
+      
+      switch (this.operation) {
+         case '+':
+            computation = prev + current;
+            break;
+         case '-':
+            computation = prev - current;
+            break;
+         case '*':
+            computation = prev * current;
+            break;
+         case '÷':
+            computation = prev / current;
+            break;
+         default:
+            return;
+      };
+
+      this.currentOperand = computation;
+      this.operation = undefined;
+      this.previousOperand = '';
    };
 
-   function delAllElements() {
-      currentOperation.innerText = '';
+   updateDisplay() {
+      this.currentOperandTextElement.innerText =
+         this.getDisplayNumber(this.currentOperand);
+      
+      if (this.operation != null) {
+         this.previousOperandTextElement.innerText =
+            `${this.getDisplayNumber(this.previousOperand)}${this.operation}`;
+      } else {
+         this.previousOperandTextElement.innerText = '';
+      };
    };
+};
 
-   numButtons.forEach(button => {
-      button.addEventListener('click', function () {
-         valueAssignment(this.value);
-         outputOnScreen(this.value);
-         console.log(inputNumber);
-      });
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('.output__previous-operand');
+const currentOperandTextElement = document.querySelector('.output__current-operand');
+const resultOutputElement = document.querySelector('.calculator__result');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+   button.addEventListener('click', () => {
+      console.log('num');
+      calculator.appendNumber(button.innerText)
+      calculator.updateDisplay();
    });
+});
 
-   operationButtons.forEach(operant => {
-      operant.addEventListener('click', function () {
-         outputOnScreen(this.value);
-         inputNumber = null;
-      });
+operationButtons.forEach(button => {
+   button.addEventListener('click', function () {
+      console.log('operator');
+      calculator.chooseOperation(button.innerText);
+      calculator.updateDisplay();
    });
+});
 
-   delOneEl.onclick = function () {
-      delOneElement();
-   }
+equalsButton.addEventListener('click', button => {
+   calculator.compute();
+   calculator.updateDisplay();
+});
 
-   delAllEl.onclick = function () {
-      delAllElements();
-   }
+allClearButton.addEventListener('click', button => {
+   calculator.clear();
+   calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+   calculator.delete();
+   calculator.updateDisplay();
 });
