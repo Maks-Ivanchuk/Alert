503db9be6fdb6edda8296c2aa2ea94362d9f719f
Maks Ivanchuk <maksivanchu@gmail.com>
2023-08-01 16:15:03 +0300

    fgsg

--- src/script.js
+++ src/script.js
@@ -1,195 +1,86 @@
-document.addEventListener("DOMContentLoaded", () => {
-   const numberButtons = document.querySelectorAll('[data-number]');
-   const pointButton = document.querySelector('[data-point]');
-   const operationButtons = document.querySelectorAll('[data-operation]');
-   const equalsButton = document.querySelector('[data-equals]');
-   const deleteButton = document.querySelector('[data-delete]');
-   const allClearButton = document.querySelector('[data-all-clear]');
-   const previousOperandTextElement = document.querySelector('.output__previous-operand');
-   const operationElement = document.querySelector('.output__operation');
-   const currentOperandTextElement = document.querySelector('.output__current-operand');
+// const s = 'd3b9UUYE1Ta3b9l7he81';
+// const p1 = 'd3b9UUYE1T';
+// const p2 = 'a3b9l7he81';
 
-   let currentOperand;
-   let previousOperand;
-   let operation;
+// const s = 'z2gzezrRXczeqI4UDou';
+// const p1 = 'z2zrRce4U';
+// const p2 = 'gezXzqIDou';
 
-   numberButtons.forEach(numberBtn => {
-      numberBtn.addEventListener('click', () => {
+// const s = '612j4F4D3JIaRlamEVa';
+// const p1 = '62443IRaEa';
+// const p2 = '1jFDJalmV';
 
-         if (currentOperand == '0') return; // якщо вже введено один 0 то більше не вводимо
-         if (currentOperandTextElement.innerHTML.includes('%')) return; // якщо в поточному стоїсть знак % то цифри більше не працюють
+// const s = ' Hacker News! ';
+// const p1 = ' cker s ';
+// const p2 = 'HaNew!';
 
-         if (currentOperand == undefined) {
-            currentOperand = numberBtn.value
-         } else {
-            currentOperand += numberBtn.value;
-         };
+const s = 'radency';
+const p1 = 'rdae';
+const p2 = 'ncy';
 
-         currentOperandTextElement.innerHTML = currentOperand;
-      });
-   });
+// const s = 'Do you have a cup of tea? Yes, I do!';
+// const p1 = ' yo ha a cof tea Yes I';
+// const p2 = 'Douveup ?, do!';
 
-   operationButtons.forEach(operationBtn => {
-      operationBtn.addEventListener('click', (event) => {
-         let target = event.target;
+// const s = 'd3b9UUYE1Ta3b9l7he81';
+// const p1 = 'd3b9UUYE1T';
+// const p2 = 'a3b9l7he81';
 
-         if (currentOperand == "0.") return;
+// const s = 'd3b9UUYE1Ta3b9l7he81';
+// const p1 = 'd3b9UUYE1T';
+// const p2 = 'a3b9l7he81';
 
-         if (target.value == "-") {
-            if (currentOperand == undefined) { //якщо знач порожгє, і клацнули "-" то робимо мінусове значення
-               currentOperand = target.value;
-               currentOperandTextElement.innerHTML = currentOperand;
-               return;
-            } else if (currentOperand == '-') { //якщо знач мінусовк, і клацнули "-" то робимо плюсове значення
-               currentOperand = undefined;
-               currentOperandTextElement.innerHTML = '';
-               return;
-            };
-         };
+function stringChecker(s, p1, p2) {
+   s = s.replace(/ /g, '');
+   p1 = p1.replace(/ /g, '');
+   p2 = p2.replace(/ /g, '');
 
-         if (currentOperand == undefined && previousOperand == undefined) return;
-         if (currentOperandTextElement.innerHTML != '' &&
-            currentOperandTextElement.innerHTML.lastIndexOf('.') ==
-            currentOperandTextElement.innerHTML.length - 1) return;
+   if ((p1.length + p2.length) - s.length != 0) return false;
 
-         if (previousOperand != undefined &&
-            currentOperand != undefined &&
-            operation != undefined &&
-            target.value == "%") {
-            if (currentOperandTextElement.innerHTML.includes('%')) return; //якщо все заповнено і клацнули %
+   // let result = '';
+   // debugger
+   // for (let i = 0; i < s.length; i++) {
+   //    for (let j = 0; j < p1.length; j++) {
+   //       if (s[i] == p1[j]) {
+   //          result += p1[j];
+   //       };
+   //    };
 
-            currentOperandTextElement.innerHTML += operationBtn.value;
-         };
+   //    for (let k = 0; k < p2.length; k++) {
+   //       if (s[i] == p2[k]) {
+   //          result += p2[k];
+   //       };
+   //    };
+   // };
 
-         if (target.value == "%") return;
+   // if (result == s) {
+   //    return true;
+   // } else {
+   //    return false;
+   // };
+   //--------------------------------------------------------
 
-         if (previousOperand == undefined) {
-            previousOperand = currentOperand;
-            previousOperandTextElement.innerHTML = previousOperand;
-            operation = target.value;
-            operationElement.innerHTML = operation;
-            currentOperandTextElement.innerHTML = '';
-            currentOperand = undefined;
-         } else {
-            operation = target.value;
-            operationElement.innerHTML = operation;
-         };
-      });
-   });
 
-   pointButton.addEventListener('click', () => {
-      if (currentOperandTextElement.innerHTML.includes('.')) return;
+   let result = '';
 
-      if (currentOperandTextElement.innerHTML == "") { //якщо пусто і натиснути на крапку то буде '0.'
-         currentOperand = '0.';
-         currentOperandTextElement.innerHTML = currentOperand;
-      } else {
-         currentOperand += '.';
-         currentOperandTextElement.innerHTML = currentOperand;
+   debugger
+   for (let i = 0; i < s.length; i++) {
+      if (p1.includes(s[i])) {
+         p1.indexOf(s[i]);
+         result += p1[p1.indexOf(s[i])];
+      } else if (p2.includes(s[i])) {
+         result += p2[p2.indexOf(s[i])];
       };
-   });
-
-   deleteButton.addEventListener('click', () => {
-      if (currentOperand == undefined) return;
-
-      currentOperandTextElement.innerHTML = currentOperandTextElement.innerHTML.slice(0, -1);
-      currentOperand = currentOperandTextElement.innerHTML;
-      if (currentOperand == '') {
-         currentOperand = undefined;
-      }
-   });
-
-   allClearButton.addEventListener('click', () => {
-      previousOperandTextElement.innerHTML = '';
-      currentOperandTextElement.innerHTML = '';
-      operationElement.innerHTML = '';
-      currentOperand = undefined;
-      previousOperand = undefined;
-      operation = undefined;
-   });
-
-   equalsButton.addEventListener('click', () => {
-      if (currentOperand == undefined || previousOperand == undefined) return;
-      if (currentOperandTextElement.innerHTML.lastIndexOf('.') ==
-         currentOperandTextElement.innerHTML.length - 1) return; //якщо остання "." то = не працює
-
-      let res;
-
-      if (currentOperandTextElement.innerHTML.includes('%')) {
-         res = interest(previousOperand, currentOperand, operation);
-
-         previousOperandTextElement.innerHTML = '';
-         currentOperandTextElement.innerHTML = '';
-         operationElement.innerHTML = '';
-         currentOperand = res;
-         previousOperand = undefined;
-         operation = undefined;
-
-         currentOperandTextElement.innerHTML = currentOperand;
-         return;
-      };
-
-      res = compute(previousOperand, currentOperand, operation);
-      previousOperandTextElement.innerHTML = '';
-      currentOperandTextElement.innerHTML = '';
-      operationElement.innerHTML = '';
-      currentOperand = res;
-      previousOperand = undefined;
-      operation = undefined;
-
-      currentOperandTextElement.innerHTML = currentOperand;
-   });
-
-   function compute(a, b, operation) {
-      let result;
-      switch (operation) {
-         case "+":
-            result = Number(a) + Number(b);
-            break;
-         case "-":
-            result = Number(a) - Number(b);
-            break;
-         case "/":
-            result = Number(a) / Number(b);
-            break;
-         case "*":
-            result = Number(a) * Number(b);
-            break;
-         default:
-            return
-      };
-
-      if (String(result).length > 10) {
-         result = result.toFixed(1);
-      };
-
-      return result;
    };
 
-   function interest(a, b, operation) {
-      let result;
-      switch (operation) {
-         case "+":
-            result = Number(a) + ((Number(a) * Number(b)) / 100); //+
-            break;
-         case "-":
-            result = Number(a) - ((Number(a) * Number(b)) / 100); // +
-            break;
-         case "/":
-            result = (Number(a) / Number(b)) * 100; //+
-            break;
-         case "*":
-            result = (Number(a) * Number(b)) / 100; //+
-            break;
-         default:
-            return
-      };
+   console.log(result);
 
-      if (String(result).length > 10) {
-         result = result.toFixed(1);
-      };
-
-      return result;
+   if (result == s) {
+      return true;
+   } else {
+      return false;
    };
-});
+};
+
+console.log(stringChecker(s, p1, p2));
 
